ad968b2d1f69a66696b8b08de766aa6bd256b988
littleGreyHui <1091557104@qq.com>
2016-09-09 16:27:23 +0800

    初始化
    Signed-off-by: littleGreyHui <1091557104@qq.com>

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by 小灰灰 on 16/9/9.
+//  Copyright © 2016年 小灰灰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by 小灰灰 on 16/9/9.
+//  Copyright © 2016年 小灰灰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
