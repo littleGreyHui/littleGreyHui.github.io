3e3576d182342b68cae39160b14df3ca5c34276f
littleGreyHui <1091557104@qq.com>
2016-09-09 16:18:52 +0800

    no message

---
+++ weiboDemo/weiboDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weiboDemo
+//
+//  Created by 小灰灰 on 16/9/9.
+//  Copyright © 2016年 小灰灰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
